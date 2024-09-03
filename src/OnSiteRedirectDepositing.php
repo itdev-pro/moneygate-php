f0bc8db5ceef93193f3b00631d7f92b418f7ea19
homichuk.mihail <homichuk.mihail@gmail.com>
2024-09-03 10:22:18 +0300

    create on-site

--- src/OnSiteRedirectDepositing.php
+++ src/OnSiteRedirectDepositing.php
@@ -11,5 +11,58 @@ use sdk_moneygate\BaseClass;
 
 class OnSiteRedirectDepositing extends BaseClass
 {
+    public function create(
+        string $callbackUrl = "https://merchant-side.com/send-status-here",
+        string $success_url = "https://front-merchant-side.com/for-successfull-payment",
+        string $fail_url = "https://front-merchant-side.com/for-failture-payment",
+
+        int $amount = 100,
+        string $currency = "RUB",
+        string $paymentType = "card2card",
+        string $bank = '',
+        string $customer_id = '',
+        $value = null,
+    ): array {
+        $this->setCallbackUrl($callbackUrl);
+        $this->setAmount($amount);
+        $this->setCurrency($currency);
+        $paymentInstrument = [
+            "payment_type" => $paymentType,
+            "bank" => $bank,
+        ];
+
+        $customerData = [
+            "customer_id" => $customer_id,
+        ];
+        $data = [
+            "value" => $value,
+            "data" => [
+            "callback_url" => $this->getCallbackUrl(),
+            "success_url" => $success_url,
+            "fail_url" => $fail_url,
+            "amount" => $this->getAmount(),
+            "currency" => $this->getCurrency(),
+            "payment_instrument" => $paymentInstrument,
+            "customer_data" => $customerData,
+        ]];
+        $this->updateData($data);
+        $this->setMethod("POST");
+        $context = stream_context_create($this->getOptions());
+        $result = file_get_contents($this->getEnviroment() . 'on-site-redirect/send', false, $context);
+        return json_decode($result, true);
+    }
+
+    public function getStatus(string $id = null)
+    {
+        if ($id) {
+            $this->setId($id);
+        }
+        $this->updateData([]);
+        $this->setMethod("POST");
+        $context = stream_context_create($this->getOptions());
+        $result = file_get_contents($this->getEnviroment() . "on-site-redirect/status", false, $context);
+        return json_decode($result, true);
+
+    }
     
 }
--- tests/OnSiteRedirectDepositing/OnSiteRedirectDepositingTest.php
+++ tests/OnSiteRedirectDepositing/OnSiteRedirectDepositingTest.php
@@ -3,11 +3,12 @@
 use Dotenv\Dotenv;
 use PHPUnit\Framework\TestCase;
 use sdk_moneygate\Auth;
-// use sdk_moneygate\OnSiteRedirectDepositing;
+use sdk_moneygate\OnSiteRedirectDepositing;
 
 class OnSiteRedirectDepositingTest extends TestCase
 {
     public $auth;
+    private static $id;
 
     protected function setUp(): void
     {
@@ -20,5 +21,25 @@ class OnSiteRedirectDepositingTest extends TestCase
     protected function tearDown(): void
     {
 
+    }
+
+    public function testOnSiteRedirectDepositingCreate(): void
+    {
+        $onSiteRedirectDepositing = new OnSiteRedirectDepositing($this->auth, true);
+        $resultCreating = $onSiteRedirectDepositing->create(customer_id: "2a1bc47b-38d2-4631-9f8c-0d497081f1ca");
+        $this->assertArrayHasKey('data', $resultCreating);
+        $this->assertArrayHasKey('id', $resultCreating);
+        $this->assertArrayHasKey('success', $resultCreating);
+        $this->assertEquals('true', $resultCreating['success']);
+        self::$id = $onSiteRedirectDepositing->getId();
+    }
+    
+    public function testOnSiteRedirectDepositingGetStatus(): void
+    {
+        $testGetStatus = new OnSiteRedirectDepositing($this->auth, true);
+        $resultGetStatusm = $testGetStatus->getStatus(self::$id);
+        $this->assertArrayHasKey('status', $resultGetStatusm);
+        $this->assertArrayHasKey('id', $resultGetStatusm);
+        $this->assertEquals(self::$id, $resultGetStatusm['id']);
     }
 }
